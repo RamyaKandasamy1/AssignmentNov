1c36c57dd4da31a5ea7225d5c9d6b3feb0e8e77f
RamyaKandasamy1 <ramz87lakshmi@yahoo.co.in>
2022-11-17 15:30:12 -0600

    second commit

--- src/labAssignment_Nov17/RevString.java
+++ src/labAssignment_Nov17/RevString.java
@@ -95,7 +95,7 @@ public class RevString {
 			
 		}
 		
-		else if((input.length()<5)||(input.length()>25)) {
+		else if((input.length()<=5)||(input.length()>=25)) {
 			throw new Exception("Invalid input enter string with length between 5 to 25 ");
 		}
 		
